d7b86de802f13fe950923bdd00120cc45d759297
oli <olidavey88@icloud.com>
2025-01-02 20:25:32 +1100

    eda figures

--- datasetEDA.py
+++ datasetEDA.py
@@ -8,4 +8,20 @@ heart_data = pd.read_csv("heart_disease.csv")
 # checking first and last 5 data rows and coloumns
 print(heart_data.head())
 print(heart_data.tail())
+print(heart_data.shape)
+
+
+print(heart_data.columns)
+# unique values
+print(heart_data.nunique())
+print(heart_data.info())
+
+# age corrolation to heart disease
+figure = plt.figure(figsize= (30, 30))
+ax = figure.gca()
+
+heart_data.hist(ax=ax, bins = 100, color = "purple")
+
+plt.show()
+# gender corrolation to heart disease
 
