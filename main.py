1fa9008f64eed949a96098cce974d67c08fa7650
oli <olidavey88@icloud.com>
2025-01-02 21:42:42 +1100

    updating all stuff

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import numpy as np
 import sklearn as sk
 import matplotlib as mp
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+import xgboost as xgb
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
+
+
 
 heart_disease_data = pd.read_csv("heart_disease.csv")
 
 predict = "Heart Disease Status"
-
-for i in range (0, len(heart_disease_data)):
-    heart_disease_data["ID"] = i
-    i += 1
-
-
 
 heart_disease_data.pop("Cholesterol Level")
 heart_disease_data.pop("Blood Pressure")
@@ -40,21 +43,55 @@ print(heart_disease_data)
 # - Sugar Consumption
 
 # Encode categorical variables
-# label_encoder = LabelEncoder()
-# heart_disease_data['Age'] = label_encoder.fit_transform(data['Age'])
-# heart_disease_data['Gender'] = label_encoder.fit_transform(data['Gender'])
-# heart_disease_data['Exercise Habits'] = label_encoder.fit_transform(data['Exercise Habits'])
-# heart_disease_data['Smoking'] = label_encoder.fit_transform(data['Smoking'])
-# heart_disease_data['Family Heart Disease'] = label_encoder.fit_transform(data['Gender'])
-# heart_disease_data['Diabetes'] = label_encoder.fit_transform(data['Diabetes'])
-# heart_disease_data['BMI'] = label_encoder.fit_transform(data['BMI'])
-# heart_disease_data['High Blood Pressure'] = label_encoder.fit_transform(data['High Blood Pressure'])
-# heart_disease_data['Alcohol Consumption'] = label_encoder.fit_transform(data['Alcohol Consumption'])
-# heart_disease_data['Stress Level'] = label_encoder.fit_transform(data['Stress Level'])
-# heart_disease_data['Sleep Hours'] = label_encoder.fit_transform(data['Sleep Hours'])
-# heart_disease_data['Sugar Consumption'] = label_encoder.fit_transform(data['Sugar Consumption'])
+le = LabelEncoder() 
+ID = le.fit_transform(list(heart_disease_data['ID']))
+age = le.fit_transform(list(heart_disease_data['Age']))
+gender = le.fit_transform(list(heart_disease_data['Gender']))
+exercise = le.fit_transform(list(heart_disease_data['Exercise Habits']))
+smoking = le.fit_transform(list(heart_disease_data['Smoking']))
+family = le.fit_transform(list(heart_disease_data['Family Heart Disease']))
+diabetes = le.fit_transform(list(heart_disease_data['Diabetes']))
+bmi = le.fit_transform(list(heart_disease_data['BMI']))
+alcohol = le.fit_transform(list(heart_disease_data['Alcohol Consumption']))
+stress = le.fit_transform(list(heart_disease_data['Stress Level']))
+sleep = le.fit_transform(list(heart_disease_data['Sleep Hours']))
+sugar = le.fit_transform(list(heart_disease_data['Sugar Consumption']))
+heart_disease = le.fit_transform(list(heart_disease_data['Heart Disease Status']))
 
-# # Split data into features and target
-# X = data.drop(['Rank', 'ID', 'GDP_diff', 'Country', 'Continent'], axis=1)
-# y = data['Continent']
 
+
+# Split data into features and target
+X = list(zip(age, gender, exercise, smoking, family, diabetes, bmi, alcohol, stress, sleep, sugar))
+y = list(heart_disease)
+
+# Split data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+predictive_models = []
+# Initialize models
+nb = GaussianNB()
+svc = SVC()
+gbc = GradientBoostingClassifier()
+rfc = RandomForestClassifier()
+dt = DecisionTreeClassifier()
+xgb_model = xgb.XGBClassifier()
+
+model_result = []
+model_names = []
+
+nb.fit(X_train, y_train)
+nb_preds = nb.predict(X_test)
+nb_acc = accuracy_score(y_test, nb_preds)
+
+nb.fit(X_train, y_train)
+nb_preds = nb.predict(X_test)
+nb_acc = accuracy_score(y_test, nb_preds)
+
+nb.fit(X_train, y_train)
+nb_preds = nb.predict(X_test)
+nb_acc = accuracy_score(y_test, nb_preds)
+
+nb.fit(X_train, y_train)
+nb_preds = nb.predict(X_test)
+nb_acc = accuracy_score(y_test, nb_preds)
