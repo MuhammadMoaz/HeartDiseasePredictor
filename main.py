e8ab31b7504a43597250c35ed13f1c6b639c780a
oli <olidavey88@icloud.com>
2025-01-02 19:55:50 +1100

    Getting Data In

--- main.py
+++ main.py
@@ -4,4 +4,16 @@ import sklearn as sk
 import matplotlib as mp
 
 heart_disease_data = pd.read_csv("heart_disease.csv")
+
+predict = "Heart Disease Status"
+
+heart_disease_data.pop("Cholesterol Level")
+heart_disease_data.pop("Blood Pressure")
+heart_disease_data.pop("Low HDL Cholesterol")
+heart_disease_data.pop("High LDL Cholesterol")
+heart_disease_data.pop("Triglyceride Level")
+heart_disease_data.pop("Fasting Blood Sugar")
+heart_disease_data.pop("CRP Level")
+heart_disease_data.pop("Homocysteine Level")
+
 print(heart_disease_data)
